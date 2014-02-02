85e07a878768cba8d388b317af043163f551abe6
Tim Pote <tim.pote@cicayda.com>
2014-02-02 13:17:35 -0600

    Added with-timeout macro.

--- test/clobaconjure/macro.clj
+++ test/clobaconjure/macro.clj
@@ -4,17 +4,22 @@
   `(cemerick.cljs.test/deftest ~(with-meta name (assoc (meta name) :async true))
      ~name ~@body))
 
+(defmacro with-timeout [timeout & body]
+  `(do
+     (js/setTimeout #(cemerick.cljs.test/done) ~timeout)
+     ~@body))
+
 (defmacro expect-events [src & events-expected]
-  `(js/setTimeout #(cemerick.cljs.test/done) 1000)
-  `(let [events-found# (atom [])]
-     (clobaconjure.core/subscribe!
-       ~src
-       (fn [event#]
-         (if (:end? event#)
-           (do
-             (cemerick.cljs.test/is ~'-test-ctx
-                                    (= @events-found#
-                                       (vector ~@events-expected))
-                                    "I must have a message")
-             (cemerick.cljs.test/done))
-           (swap! events-found# conj (:value event#)))))))
+  `(with-timeout 1000
+     (let [events-found# (atom [])]
+       (clobaconjure.core/subscribe!
+         ~src
+         (fn [event#]
+           (if (:end? event#)
+             (do
+               (cemerick.cljs.test/is ~'-test-ctx
+                                      (= @events-found#
+                                         (vector ~@events-expected))
+                                      "I must have a message")
+               (cemerick.cljs.test/done))
+             (swap! events-found# conj (:value event#))))))))
